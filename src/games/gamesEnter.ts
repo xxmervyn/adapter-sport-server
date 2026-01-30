48ebd882e07031f01ad704bbec4d287fa4c0af60
xxmervyn <154972891+xxmervyn@users.noreply.github.com>
2026-01-30 16:22:37 +0800

    t

--- src/games/gamesEnter.ts
+++ src/games/gamesEnter.ts
@@ -30,8 +30,9 @@ export class GamesEnterEndpoint extends OpenAPIRoute {
 		const apiHostName = urlReq?.hostname.replace(".","-api.");
 		//https://fbsports.appplaygasdsd.org/index.html?token=tt_Eqszk6rfTLJ0k557ieBGbn4utAF86jDB.c5b56b065029f573de29c77477762f4e&nickname=t012&pcAddress=https://c.e70cz.com&virtualSrc=https://v.api.fbs01.com&apiSrc=https://api.a233z1.com&pushSrc=wss://push.5890v.com&platformName=FB%E4%BD%93%E8%82%B2&icoUrl=https://ns-client.newsportspro.com/favicon.ico&handicap=1&themeBg=4C6FFF&themeText={%22h5FgColor%22:%22#/undefined?pcAddress=https%3A%2F%2Fc.e70cz.com&themeBg=4C6FFF&themeText=%7B%22h5FgColor%22%3A%22%23%2F
 		// return c.redirect(`https://${urlReq?.hostname}/index.html?token=${data.query.playerGameToken}&nickname=${"nickname"}&apiSrc=https://${apiHostName}`);
-		return c.redirect(`https://${urlReq?.hostname}/index.html?token=${data.query.playerGameToken}&`+
-			`nickname=t012&pcAddress=https://${urlReq?.hostname}&virtualSrc=https://v.${apiHostName}&apiSrc=https://${apiHostName}&pushSrc=wss://push.${apiHostName}&`+
-			`platformName=FB%E4%BD%93%E8%82%B2&icoUrl=https://${urlReq?.hostname}/favicon.ico&handicap=1&themeBg=4C6FFF&themeText={%22h5FgColor%22:%22#/undefined?pcAddress=https%3A%2F%2Fc.e70cz.com&themeBg=4C6FFF&themeText=%7B%22h5FgColor%22%3A%22%23%2F`)
+		return c.redirect(`https://${urlReq?.hostname}/index.html#/?token=${data.query.playerGameToken}&nickname=t013&`+
+			`pcAddress=https://${urlReq?.hostname}&virtualSrc=https://v.${apiHostName}&apiSrc=https://${apiHostName}&pushSrc=wss://push.${apiHostName}&platformName=FB体育&icoUrl=https://${urlReq?.hostname}/favicon.ico&`+
+			`handicap=1&themeBg=4C6FFF&themeText={&quot;h5FgColor&quot;:&quot;#4C6FFF&quot;,&quot;pcFgColor&quot;:&quot;#4C6FFF&quot;,&quot;pcThemeCustomFgColor&quot;:&quot;#4C6FFF&quot;}`+
+			`&controlMenu=2&language=ZHO`)
 	}
 }
