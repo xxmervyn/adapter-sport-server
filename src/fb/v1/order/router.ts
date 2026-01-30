5e3faac7b44599030ea512fa6e32f4a4ba50e882
yuyueplus <yuyueplus@163.com>
2026-01-30 20:46:18 +0800

    update

--- src/fb/v1/order/router.ts
+++ src/fb/v1/order/router.ts
@@ -5,5 +5,5 @@ import { FbV1OrderCashOutRouter } from "./cashOut/router";
 export const FbV1OrderRouter = fromHono(new Hono());
 
 
-FbV1OrderRouter.route("/user",FbV1OrderNewRouter);
+FbV1OrderRouter.route("/new",FbV1OrderNewRouter);
 FbV1OrderRouter.route("/cashOut",FbV1OrderCashOutRouter);
