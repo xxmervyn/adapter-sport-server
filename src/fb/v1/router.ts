c5d0f4c5320d90006df75442e3a39442ecb679ee
yuyueplus <yuyueplus@163.com>
2026-01-30 20:19:02 +0800

    update

--- src/fb/v1/router.ts
+++ src/fb/v1/router.ts
@@ -5,6 +5,7 @@ import { FbV1MerchantRouter } from "./merchant/router";
 import { FbV1MatchRouter } from "./match/router";
 import { FbV1BulletinRouter } from "./bulletin/router";
 import { FbV1OrderNewRouter } from "./order/new/router";
+import { FbV1SportRuleRouter } from "./sportRule/router";
 export const FbV1Router = fromHono(new Hono());
 
 
@@ -13,3 +14,4 @@ FbV1Router.route("/merchant",FbV1MerchantRouter);
 FbV1Router.route("/match", FbV1MatchRouter);
 FbV1Router.route("/bulletin", FbV1BulletinRouter);
 FbV1Router.route("/order/new", FbV1OrderNewRouter);
+FbV1Router.route("/sportRule", FbV1SportRuleRouter);
