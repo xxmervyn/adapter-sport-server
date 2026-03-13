e19796bd19331b6f853ec4bf5bcc0410d294a09f
Xxpyaojiu <1186009600@qq.com>
2026-03-13 20:45:58 +0800

    修改host

--- src/fb/enums/apiBaseUrlEnum.ts
+++ src/fb/enums/apiBaseUrlEnum.ts
@@ -1,4 +1,4 @@
 export enum API_BASE_URL_ENUMS{
     HTTPS_API_A233Z1_COM = "https://api.a233z1.com",
-    FORWARD_COM = "https://inner2fbsports-api.appplaygasdsd.org",
+    FORWARD_COM = "http://inner2fbsports-api.appplaygasdsd.org",
 }
