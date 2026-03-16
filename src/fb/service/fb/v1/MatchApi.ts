b537b51a4aed385ad7bd7757726b754d5c8c45d0
Xxpyaojiu <1186009600@qq.com>
2026-03-16 23:51:39 +0800

    接口调整

--- src/fb/service/fb/v1/MatchApi.ts
+++ src/fb/service/fb/v1/MatchApi.ts
@@ -1,56 +1,56 @@
-import { HonorequestWithNotAuth } from "hono"
+import { HonoRequest } from "hono"
 import { FbServiceEntry } from "../../entry/service/fbServiceEntry"
 
 export class V1MatchApi {
-    public getList(params: any, req: HonorequestWithNotAuth) {
+    public getList(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/getList', params, req)
     }
     
-    public tips(params: any, req: HonorequestWithNotAuth) {
+    public tips(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/tips', params, req)
     }
 
-    public matchCount(params: any, req: HonorequestWithNotAuth) {
+    public matchCount(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/matchCount', params, req)
     }
 
-    public getOnSaleLeagues(params: any, req: HonorequestWithNotAuth) {
+    public getOnSaleLeagues(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/getOnSaleLeagues', params, req)
     }
 
-    public getMatchDetail(params: any, req: HonorequestWithNotAuth) {
+    public getMatchDetail(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/getMatchDetail', params, req)
     }
 
-    public getBannerMatchList(params: any, req: HonorequestWithNotAuth) {
+    public getBannerMatchList(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/getBannerMatchList', params, req)
     }
 
-    public statistical(params: any, req: HonorequestWithNotAuth) {
+    public statistical(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/statistical', params, req)
     }
     
-    public matchResultPage(params: any, req: HonorequestWithNotAuth) {
+    public matchResultPage(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/matchResultPage', params, req)
     }
 
-    public matchResultInfo(params: any, req: HonorequestWithNotAuth) {
+    public matchResultInfo(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/matchResultInfo', params, req)
     }
 
-    public recommendMatchList(params: any, req: HonorequestWithNotAuth) {
+    public recommendMatchList(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/recommendMatchList', params, req)
     }
 
-    public queryMatchByRecommend(params: any, req: HonorequestWithNotAuth) {
+    public queryMatchByRecommend(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/queryMatchByRecommend', params, req)
     }
 
-    public getLiveList(params: any, req: HonorequestWithNotAuth) {
+    public getLiveList(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match/getLiveList', params, req)
     }
    
-    public listMarketGroup(params: any, req: HonorequestWithNotAuth) {
+    public listMarketGroup(params: any, req: HonoRequest) {
         return FbServiceEntry.request('/v1/match//listMarketGroup', params, req)
     }
 
