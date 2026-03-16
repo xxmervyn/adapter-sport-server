9ab2b9feef695c358c6cbbf2fc5338093690b7c0
Xxpyaojiu <1186009600@qq.com>
2026-03-17 01:07:37 +0800

    接口调整

---
+++ src/fb/api/v1/bulletin/listPage.ts
@@ -0,0 +1,40 @@
+import { OpenAPIRoute, contentJson } from "chanfana";
+import { z } from "zod";
+import { FbService } from "../../../service/fbService";
+import { AppContext } from "../../../../types";
+
+export class V1BulletinListPage extends OpenAPIRoute {
+    public schema = {
+        tags: ["V1BulletinListPage"],
+        summary: "V1BulletinListPage",
+        operationId: "V1BulletinListPage",
+        request: {
+            body: contentJson(
+                z.object({
+                    current: z.number().optional(),
+                    size: z.number().optional(),
+                    orders: z.array(z.any()).optional(),
+                    languageType: z.string(),
+                    sportId: z.number().optional(),
+                    startTime: z.number().optional(),
+                    endTime: z.number().optional(),
+                })
+            ),
+        },
+        responses: {
+            "200": {
+                description: "V1BulletinListPage",
+                ...contentJson({
+                    code: z.any(),
+                    data: z.any(),
+                    success: z.boolean(),
+                }),
+            },
+        },
+    };
+
+    async handle(c: AppContext) {
+        const data = await this.getValidatedData<typeof this.schema>();
+        return FbService.V1Bulletin.listPage(data.body, c.req)
+    }
+}
--- src/fb/api/v1/bulletin/router.ts
+++ src/fb/api/v1/bulletin/router.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { fromHono } from "chanfana";
 import { V1BulletinListTop } from "./listTop";
+import { V1BulletinListPage } from "./listPage";
 
 export const FbV1BulletinRouter = fromHono(new Hono());
 FbV1BulletinRouter.post("/listTop", V1BulletinListTop);
+FbV1BulletinRouter.post("/listPage", V1BulletinListPage);
