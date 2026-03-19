635c28b4d123157ed83b7472a7d029f40cd6d0b3
Xxpyaojiu <1186009600@qq.com>
2026-03-19 18:14:44 +0800

    接口调整

--- src/games/gamesEnter.ts
+++ src/games/gamesEnter.ts
@@ -62,9 +62,12 @@ export class GamesEnterEndpoint extends OpenAPIRoute {
 			url = `https://${urlReq?.hostname}/index.html#/?token=${data.query.playerGameToken}&nickname=${info?.UserName}&` +
 				`pcAddress=https://${urlReq?.hostname}&virtualSrc=https://${apiHostName}&apiSrc=https://${apiHostName}&pushSrc=wss://push.5890v.com&icoUrl=https://${urlReq?.hostname}/favicon.ico&` +
 				`handicap=1&themeBg=4C6FFF&themeText=${themeText}&controlMenu=2&language=${lang}`
-			const sginUrl = `https://${urlReq?.hostname}?token=${data.query.playerGameToken}` 
+
+			const sginUrl = `https://${urlReq?.hostname}?token=${data.query.playerGameToken}`
 			var xfontpage = genGameUrlSignWithKeys(data.query, sginUrl, ["token"], true)
-			const tokenInfo = await UserService.V1User.token(xfontpage, data.query.playerGameToken);
+			const tokenInfo = await UserService.V1User.token(xfontpage, data.query.playerGameToken)
+
+			url = genGameUrlSignWithKeys(data.query, sginUrl, ["token"], true)
 			url = `${url}&tk=${tokenInfo.token}`
 		}
 
