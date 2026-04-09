e7841e8dfed846ee677cd396cc2e80119ecdf566
Xxpyaojiu <1186009600@qq.com>
2026-04-09 16:09:25 +0800

    接口调整

--- src/games/gamesEnter.ts
+++ src/games/gamesEnter.ts
@@ -83,13 +83,11 @@ export class GamesEnterEndpoint extends OpenAPIRoute {
 			const userReq = new Request('http://localhost/user', {
 				method: 'POST',
 				headers: {
-					'x-front-page': sginUrl,
-					'authorization': tokenInfo.token,
+					'X-Front-Page': xfontpage,
+					'Authorization': tokenInfo.token,
 				}
 			})
 			const userHonoReq = new HonoRequest(userReq)
-			console.log("!!!!!!!!!!!!!!!!", userHonoReq.header("x-front-page"))
-			console.log("!!!!!!!!!!!!!!!!", userHonoReq.header("authorization"))
 			const userInfo = await UserService.V1User.userInfo({}, userHonoReq)
 			if (userInfo.code != 0) {
 				return {
